a1f4e3e69339b7114f109ac234b326e587a63e56
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-01 10:24:36 -0700

    Add views and create routes for them

--- auth-client/src/App.js
+++ auth-client/src/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import { Route } from "react-router-dom";
+
+import HomeView from "./views/HomeView";
+import LoginView from "./views/LoginView";
+import RegisterView from "./views/RegisterView";
+
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>App</h1>
-      </div>
-    );
-  }
-}
+const App = props => {
+  return (
+    <div className="App">
+      <Route exact path="/" render={props => <HomeView {...props} />} />
+      <Route path="/login" render={props => <LoginView {...props} />} />
+      <Route path="/register" render={props => <RegisterView {...props} />} />
+    </div>
+  );
+};
 
 export default App;
